8478beef355916208354369dcf399edd6700e7ce
Aizhan <aizh.aidarova@gmail.com>
2022-03-16 14:02:00 +0600

    hw react 16_03

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Content from "./Content";
+import ContentHeading from "./Content Heading";
+import ContentEntry from "./Content Entry";
+import ContentName from "./ContentName";
+import ContentPosts from "./ContentPosts";
+import ContentTags from "./ContentTags";
+
 
 const App = () =>{
   return(
       <div className="w3-content" style={{maxWidth: '1400px'}}>
           <Header/>
-          <Content/>
+          <div className="w3-row">
+              <div  className="w3-col l8 s12">
+                  <ContentHeading/>
+                  <ContentEntry/>
+              </div>
+              <div className="w3-col l4">
+                  <ContentName/>
+                  <ContentPosts/>
+                  <ContentTags/>
+              </div>
+          </div>
           <Footer/>
       </div>
   )
